8d9f1dc01b061dfb84cd35e74cf67a1f3c1c4b25
Li Che Hsu <112653837+Liamhsu0504@users.noreply.github.com>
2023-04-26 03:00:55 -0700

    Update evaluator.pl
    update declare and assign

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -1,40 +1,79 @@
-program_eval(t_program(X), Result) :-
-    eval_block(X, [], Result).
+program_eval(t_program(P), VAL) :-
+    eval_block(P, [], VAL).
 
-eval_block(t_block(T1), ENV, Result) :-
-    eval_declaration(T1, ENV, NewENV).
-    %eval_command(T2, NewENV, NewENV1),
-	lookup(z,NewENV,Result).
+eval_block(t_block(T1), ENV, VAL) :-
+    eval_declaration(T1, ENV, NewENV),
+	lookup(Z,NewENV,VAL).
 
 eval_declaration(t_declare(T1, T2),ENV,NewENV) :- 
     eval_declaration(T1, ENV, NewENV1),
+    write("Test"),
     eval_declaration(T2, NewENV1, NewENV).
 
+
+eval_declaration(t_const(T1, T2), ENV, NewENV) :-
+    write("assign int "),
+    (   T2 = t_digit(Num) % Check if T2 is a numeric literal
+    ->  eval_num(T1, ENV, Id),
+        notContain(Id, ENV),
+        add(Id, _, ENV, NewENV),
+        update(Id, Num, ENV, NewENV)
+    ;   write("ERROR: t_const second argument is not a numeric literal")
+    ).
+
+eval_declaration(t_string(T1, T2), ENV, NewENV) :-
+    write("assign string "),
+    (   T2 = t_string(Str) % Check if T2 is a string literal
+    ->  eval_num(T1, ENV, Id),
+        notContain(Id, ENV),
+        add(Id, _, ENV, NewENV1),
+        update(Id, Str, ENV, NewENV)
+    ;   write("ERROR: t_string second argument is not a string literal")
+    ).
+eval_declaration(t_bool(T1, T2), ENV, NewENV) :-
+    write("assign boolean "),
+    (   T2 = t_boolean(Bool) % Check if T2 is a string literal
+    ->  eval_num(T1, ENV, Id),
+        notContain(Id, ENV),
+        add(Id, _, ENV, NewENV1),
+        update(Id, Bool, ENV, NewENV)
+    ;   write("ERROR: t_bool second argument is not a boolean literal")
+    ).
+
 eval_declaration(t_const(T),ENV, NewENV) :- 
+    eval_num(T,ENV,Id), 
+    write(Id),
+    notContain(Id, ENV),
+    add(Id,_,ENV, NewENV).
+
+
+eval_declaration(t_string(T),ENV, NewENV) :- 
+    write("test_string"),
     eval_num(T,ENV,Id), 
     notContain(Id, ENV), 
     add(Id,_,ENV, NewENV).
 
-eval_declaration(t_const(T1, T2), ENV, NewENV) :- eval_num(T1,ENV,Id), eval_num(T2,ENV,Num), update(Id,Num,ENV,NewENV).
-eval_declaration(t_string(T1, T2), ENV, NewENV) :- eval_num(T1,ENV,Id), eval_num(T2,ENV,Num), update(Id,Num,ENV,NewENV).
-eval_declaration(t_bool(T1, T2), ENV, NewENV) :- eval_num(T1,ENV,Id), eval_num(T2,ENV,Num), update(Id,Num,ENV,NewENV).
-
-eval_declaration(t_const(_T),ENV, ENV).   
+eval_declaration(t_const(_T),ENV, ENV).  
 eval_declaration(t_string(_T),ENV, ENV).
 eval_declaration(t_bool(_T),ENV, ENV).
 
-eval_num(t_var(X),_PAR, X).
-eval_num(t_digit(I),_PAR,I).
-eval_num(t_string(I),_PAR,I).
+eval_num(t_var(X),_ENV,X).
+eval_num(t_digit(I),_ENV,I).
+eval_num(t_string(S),_ENV,S).
 
-add(K, PAR,L,[(K, PAR)|L]).
+lookup(Id,[(Id,Val)|_], Val).
+lookup(Id,[_|T], Val):-lookup(Id,T, Val).
+lookup(Id,[],_Val):-write(Id),write(" not exist.").
 
-lookup(K,[(K,INT)|_], INT).
-lookup(K,[_|T], INT):-lookup(K,T, INT).
+update(Id, Val, [], [(Id, Val)]).
+update(Id, Val, [(Id,_)|T], [(Id, Val)|T]).
+update(Id, Val, [H|T], [H|R]) :- H\=(Id,_),update(Id,Val,T,R), write("update3").
 
-update(K, INT, [], [(K, INT)]).
-update(K, INT, [(K,_)|T], [(K, INT)|T]).
-update(K, INT, [H|L], [H|B]) :- H\=(K,_),update(K,INT,L,B).
+notContain(_Id, []).
+notContain(Id, [(Id,_)|_]) :-
+    throw(duplicate_variable_name(Id)).
+notContain(Id, [(Id1,_)|T]) :-
+    Id \= Id1,
+    notContain(Id, T).
 
-notContain(_Id,[]).
-notContain(Id,[(Id1,_)|T]):- Id\=Id1, notContain(Id,T).
+add(Id, NewVal,L,[(Id, NewVal)|L]).
