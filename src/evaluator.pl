590cee4d5e092df6ed9462b09cb2641d7f8e0a6d
Li Che Hsu <112653837+Liamhsu0504@users.noreply.github.com>
2023-04-27 00:09:35 -0700

    update operations

--- src/evaluator.pl
+++ src/evaluator.pl
@@ -49,8 +49,7 @@ eval_declaration(t_bool(T1, T2), ENV, Env) :-
 eval_declaration(t_const(T),ENV, NewENV) :- 
     eval_num(T,ENV,Id), 
     notContain(Id, ENV),
-    add(Id,_,ENV, NewENV),
-    write(NewENV), nl.
+    add(Id,_,ENV, NewENV).
 
 eval_declaration(t_string(T),ENV, NewENV) :- 
     eval_num(T,ENV,Id), 
@@ -69,16 +68,25 @@ eval_declaration(t_bool(_T),ENV, ENV).
 eval_num(t_var(X),_ENV,X).
 eval_num(t_digit(X),_ENV,X).
 eval_num(t_string(X),_ENV,X).
+eval_num(t_bool(X),_ENV,X).
 
-com_eval(t_command(X,Y),Env,NewEnv) :- com1_eval(X,Env,NEnv), com_eval(Y,NEnv,NewEnv).
-com_eval(X,Env,NewEnv) :- com1_eval(X,Env,NewEnv).
+com_eval(t_command(X,Y),Env,NewEnv) :- 
+     write('t_command Env: '), write(Env), nl,
+    com1_eval(X,Env,NEnv),
+     write('t_command NEnv: '), write(NEnv), nl,
+    com_eval(Y,NEnv,NewEnv).
+com_eval(X,Env,NewEnv) :- write('com_eval '), nl,
+    com1_eval(X,Env,NewEnv).
 
 %assignment evaluation
-com1_eval(t_assign(X,Y),Env,NewEnv) :- 
+com1_eval(t_assign(X,Y),Env,NewEnv) :-
+    %write("test assign"), nl,
     eval_num(X, Env, Id), nl,
     write('X: '), write(X), nl,
     write('Id: '), write(Id), nl,
-    lookup(Id, Env, _),
+    write('Env: '), write(Env), nl,
+    %lookup(Id, Env, _),
+    write('Y: '), write(Y), nl,
     expr_eval(Y,Env,NEnv,Val),
     write('Val: '), write(Val), nl,
     write('NEnv: '), write(NEnv), nl,
@@ -129,26 +137,45 @@ bool_eval(t_booleanNotEquality(X),Env,false):- bool_eval(X,Env,Bool), Bool = tru
 %new evaluate
 expr_eval(t_digit(X),ENV,ENV,X).
 expr_eval(t_string(X),ENV,ENV,X).
-expr_eval(t_bool(X),ENV,ENV,X).
+expr_eval(t_boolean(X),ENV,ENV,X).
 
 %evaluate the expression
+expr_eval(X, Env, Env, Val) :- 
+    %write("Env "), write(Env), nl,
+    %write("X "), write(X), nl,
+    eval_num(X, Env, Id),
+    %write("Id"), write(Id), nl,
+    lookup(Id, Env, Val).
+    %write("Val"), write(Val), nl,
+    %write(Val).
 expr_eval(X, Env, Env, Val) :- temp1_eval(X, Env, Env, Val).
-expr_eval(t_add(X,Y), Env, NewEnv, Val) :- expr_eval(X, Env, NEnv,V1),
-    temp1_eval(Y, NEnv, NewEnv,V2), Val is V1 + V2.
+expr_eval(t_add(X,Y), Env, NewEnv, Val) :-
+    write("add "), write(X), write(Y), nl,
+    expr_eval(X, Env, NEnv,V1),
+    temp1_eval(Y, NEnv, NewEnv,V2), 
+    Val is V1 + V2.
 expr_eval(t_sub(X,Y), Env, NewEnv, Val) :- expr_eval(X, Env, NEnv,V1), 
     temp1_eval(Y, NEnv, NewEnv,V2), Val is V1 - V2.
 
 %evaluate temp1 expression
+temp1_eval(t_digit(X),Env,Env,X):-write("digit "), write(X), nl.
 temp1_eval(T,Env,Env,Val) :- temp2_eval(T,Env,Env,Val).
+temp1_eval(T,Env,Env,Val) :-
+    eval_num(T, Env, Id),
+    lookup(Id, Env, Val).
 temp1_eval(t_multiply(X,Y), Env, NewEnv, Val) :- temp1_eval(X, Env, NEnv,Val1),
     temp2_eval(Y,NEnv,NewEnv,Val2),Val is Val1 * Val2.
 temp1_eval(t_divide(X,Y), Env, NewEnv, Val) :- temp1_eval(X, Env, NEnv,Val1),
     temp2_eval(Y,NEnv,NewEnv,Val2),Val is Val1 / Val2.
 
 %evaluate temp2 expression
+temp2_eval(t_digit(X),Env,Env,X):- write("digit "), write(X), nl.
 temp2_eval(t_parenthesis(X),Env,NewEnv,Val) :- expr_eval(X,Env,NewEnv,Val).
 temp2_eval(t_assign(X,Y),Env,NewEnv,Val) :- expr_eval(Y,Env,NEnv,Val),
     update(X,NEnv,Val,NewEnv).
+temp2_eval(T,Env,Env,Val) :-
+    eval_num(T, Env, Id),
+    lookup(Id, Env, Val).
 
 %evaluate relational
 relation_eval('>', X, Y, R) :- X > Y, R = true.
@@ -161,8 +188,11 @@ relation_eval(!, _, _, R) :- R = false.
 
 add(Id, NewVal,L,[(Id, NewVal)|L]).
 
-lookup(Id,[(Id,_Val)|_], Id).
-lookup(Id,[_|T], Val):-lookup(Id, T, Val),write(" lookup ").
+lookup(Id,[(Id,Val)|_], Val):- write("lookup").
+lookup(Id,[H|T], Val):-
+    write("Pair: "),write(H), nl,
+    write("Id: "),write(Id), nl,
+    lookup(Id, T, Val).
 lookup(Id,[],_Val):-write(Id),write(" not exist.").
 
 update(V,[],NewVal,[(V,NewVal)]):- write("cant not find").
@@ -175,3 +205,4 @@ notContain(Id, [(Id,_)|_]) :-
 notContain(Id, [(Id1,_)|T]) :-
     Id \= Id1,
     notContain(Id, T).
+
