c3a79bd6c3d94f61c2305dc3c90447b5cd1260c4
karanjavali <112652989+karanjavali@users.noreply.github.com>
2023-04-28 21:50:16 -0700

    Update app.component.ts
    removed console.log statements

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -44,7 +44,6 @@ export class AppComponent {
     let i=0;
     while(i<inputs.length) {
       inputs[i] = inputs[i].trim();
-      console.log(inputs[i]);
       if (inputs[i].length == 0) {
         
         inputs.splice(i,1);
@@ -54,7 +53,6 @@ export class AppComponent {
       }
     }
     this.api.post("http://127.0.0.1:5000",{args:inputs}).subscribe((res:any) => {
-      console.log(res);
       let outputs = res.res;
       this.status = res.status;
       this.output = outputs.split("\r\n");
