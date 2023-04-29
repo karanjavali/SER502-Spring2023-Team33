b40be4650d4011ea0c9bf2984bb81069499ee246
karanjavali <112652989+karanjavali@users.noreply.github.com>
2023-04-28 22:47:56 -0700

    Update app.py
    adding comments to backend app

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,7 @@ def execute_query():
     create_executables()
     input_args = request.get_json(force=True)['args']
     
-    
-
+    # Use subprocess to execute laky.exe and pass input args
     p = subprocess.Popen(['laky.exe'] + input_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Decode the output and error messages into strings
@@ -18,15 +17,17 @@ def execute_query():
     eval_error = error.decode('utf-8')
 
 
-
+    # status
     # 200 -> good
     # 400 -> errors
-
+    
+    # output object
     output_data = {
         'status': 200,
         'res': '',
     }
-
+    
+    # if there is an error, return error and change status otherwise return the query result 
     if eval_error != '':
         output_data['res'] = eval_error
         output_data['status'] = 400
@@ -36,4 +37,5 @@ def execute_query():
     return output_data
 
 def create_executables():
+    # Create exe file from pl using swipl
     subprocess.run(['swipl.exe','-o','laky.exe','-g','main','-c','../src/laky.pl'])
