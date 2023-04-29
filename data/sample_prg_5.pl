f00ba034a74f512437bedf2aee8751a76dd48e71
karanjavali <112652989+karanjavali@users.noreply.github.com>
2023-04-28 21:31:48 -0700

    Update sample_prg_5.pl

--- data/sample_prg_5.pl
+++ data/sample_prg_5.pl
@@ -1,1 +1,9 @@
-program(P,[begin, int, x, := , 10, ;, int, y, ;, bool, z,  ;, y, :=, 3, ;, tern, x, '>', y, ?, z, :=, true, :, z, :=, false, endtern, end, .],[]),write(P),program_eval(P,Z).
+begin
+int x := 10 ; 
+int y ; 
+bool z  ; 
+y := 3 ; 
+tern x > y ? z := true : z := false 
+endtern ; 
+print z 
+end.
