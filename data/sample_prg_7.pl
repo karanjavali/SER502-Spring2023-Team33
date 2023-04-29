b892fe313807fc1e4dff9cbee0f000cb7568d61c
karanjavali <112652989+karanjavali@users.noreply.github.com>
2023-04-28 21:32:42 -0700

    Update sample_prg_7.pl

--- data/sample_prg_7.pl
+++ data/sample_prg_7.pl
@@ -1,1 +1,9 @@
-program(P,[begin, int, x, :=, 0,  ;, for, i, inrange, 2, 5,'{', x, :=, x, +, 1,'}', endforpython, end, .],[]),write(P),program_eval(P,X).
+begin
+int x := 0 ;
+for i inrange 2 5 
+{
+x := x + 3;
+print x
+} 
+endforpython 
+end.
