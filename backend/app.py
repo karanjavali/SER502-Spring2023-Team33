413e2e0b832ba80e02197a1af35ec4e878ee612a
karanjavali <knjavali@asu.edu>
2023-04-28 19:12:22 -0700

    backend in progress

--- backend/app.py
+++ backend/app.py
@@ -4,16 +4,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def execute_query():
-    data = request.get_json(force=True)
+    create_executables()
+    input_args = request.get_json(force=True)['args']
+    print(input_args)
+    
+    
 
-    p = subprocess.Popen(data, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(['laky.exe'] + input_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     # Decode the output and error messages into strings
-    out, err = p.communicate()
-    out_str = out.decode('utf-8')
-    err_str = err.decode('utf-8')
+    output, error = p.communicate()
+    eval_output = output.decode('utf-8')
+    eval_error = error.decode('utf-8')
 
-    # Print the output and error messages
-    print('Output:', out_str)
-    print('Error:', err_str)
-    return out_str
+    print('parse tree -> ',eval_output)
+    print('parse tree err -> ',eval_error)
+
+    return  eval_output
+
+def create_executables():
+        subprocess.run(['swipl.exe','-o','laky.exe','-g','main','-c','../src/laky.pl'])
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -20,11 +20,22 @@ export class AppComponent {
   }
 
   onRun() {
-    let inputs = this.codeInput.value.replace( /\n /g, " " ).split( " " );
+    let replaced_str = this.codeInput.value.replaceAll(";", " ;");
+    let inputs = replaced_str.replace( /\n/g, " " ).split( " " );
     // for(let i=0;i<inputs.length;i++) {
     //   inpu
     // }
-    this.api.post("http://127.0.0.1:5000",inputs).subscribe((res:any) => {
+    let i=0;
+    while(i<inputs.length) {
+      inputs[i] = inputs[i].trim();
+      console.log(inputs[i]);
+      if (inputs[i].length == 0) {
+        
+        inputs.splice(i,1);
+      }
+      i++;
+    }
+    this.api.post("http://127.0.0.1:5000",{args:inputs}).subscribe((res:any) => {
       console.log(res);
       this.output = res.parse_tree + ', Z=' + res.output;
     })
