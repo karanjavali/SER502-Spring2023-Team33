dbd109ad959cdc3e1f076c39f313055d36e94e0d
Li Che Hsu <112653837+Liamhsu0504@users.noreply.github.com>
2023-04-28 21:07:19 -0700

    Update sample_prg_1.pl

--- data/sample_prg_1.pl
+++ data/sample_prg_1.pl
@@ -1,1 +1,8 @@
-program(P,[begin, int, x, :=, 5, ;, int, y,;,int, z, ;,y, := , 5,;,z,:=, x, +, y, end, .],[]),write(P),program_eval(P,Z).
+begin 
+  int x := 5; 
+  int y;
+  int z;
+  y := 5;
+  z:= x + y;
+  print z;
+end.
